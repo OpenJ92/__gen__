a7874fe08e88ca049cd88aac8862b0155e16a3ac
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-01-25 16:05:15 -0500

    Update Deps and Play

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::ops::Add;
 use std::ops::Mul;
 use std::ops::Sub;
+use wedged::algebra::*;
+use ndarray::Array;
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
 
 trait VectorFunction<T> {
     fn call(&self, t: Vec<T>) -> Vec<T>;
@@ -209,6 +213,26 @@ fn main() {
     VectorFunction::call(&v1, vec![1., 2., 3., 4.]);
 
     println!("{}", line.export());
+
+
+    let v1 = Vec3::new(1.0, 0.0, 0.0);
+    let v2 = Vec3::new(0.0, 1.0, 0.0);
+    let plane = v1 ^ v2;
+    let cross = plane.dual();
+    
+    assert_eq!(plane, BiVec3::new(0.0,0.0,1.0));
+    assert_eq!(cross, Vec3::new(0.0,0.0,1.0));
+
+    let binding = Array::random((3,), Uniform::new(0., 10.));
+    let [a, b, c] = match binding.as_slice()
+    {
+            Some([x, y, z, ..]) => [x, y, z],
+            _ => panic!(),
+    };
+    let t4 = BladeD::from_element(4, 3, 6.28); //dynamic dim, dynamic grade
+    println!("{:8.4}", t4);
+
+    let q = Vec3::new(a, b, c);
 
     return ();
 }
