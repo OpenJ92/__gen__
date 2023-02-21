2b607c2a40dfd616597f1a53977c78168901b462
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-02-21 10:40:21 -0500

    musing on typeclass structure and input output form

--- src/traits.rs
+++ src/traits.rs
@@ -1,10 +1,14 @@
 pub trait SampleMethod<T> {
-    fn values(&self) -> Vec<T>;
+    // As we continue to develop this software, this form should become
+    // fn values(&self) -> Vec<impl Atomic>. Atomic should be exportable
+    // forms and callable forms. With that, the VectorFunction required
+    // function call should be fn call(&self, t: impl Atomic> -> impl Atomic.
+    fn values(&self) -> Vec<Vec<T>>;
 }
 pub trait VectorFunction<T> {
     fn call(&self, t: Vec<T>) -> Vec<T>;
     fn sample(&self, sample_method: impl SampleMethod<T>) -> Vec<Vec<T>> {
-        return sample_method.values().iter().map(|t| self.call(vec!(*t))).collect()
+        return sample_method.values().iter().map(|t| self.call(*t)).collect()
     }
 }
 pub trait Exportable { 
