d0843ff504752f94bf0f6aa04cb5b5159e12f5c2
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-02-23 14:21:21 -0500

    comment on construction. We may need to change output to Result<(), ()> where call just mutates the data.

--- src/polyline.rs
+++ src/polyline.rs
@@ -56,6 +56,14 @@ where
 }
 
 impl<'a, T: ndarray::Dimension> traits::AVectorFunction<T> for PolyLine<'a, T> {
+    // There might be one more construction of the "VFF" Traits. We might want to have
+    // the call function typed like the following:
+    //  
+    //      fn call(&self, t: mut &Atom<T>) -> ()
+    //
+    //  where we simply mutate the memory at the given address. The Array components
+    //  inside the Atom types are heap allocated. Then a composition of functions applied
+    //  to the heap will update the components inplace.
     fn call(&self, t: &Atom<T>) -> Result<&Atom<T>, ()> {
         let res: Result<&Atom<T>, ()> = match t {
             scalar@Atom::Scalar { .. } => self.call_scalar(scalar),
@@ -75,20 +83,21 @@ impl<'a, T: ndarray::Dimension> traits::AVectorFunction<T> for PolyLine<'a, T> {
     fn call_line(&self, t: &Atom<T>)  -> Result<&Atom<T>, ()>{
         let res: Result<&Atom<T>, ()> = match t {
             Atom::Line { start: start, end: end } 
-                => { let resstart: Result<&Array<Ix1, T>, ()>
-                              = match self.call_point(&Atom::Point {point: start}) {
-                                        Ok(Atom::Point { point: arr }) => Ok(arr), 
-                                        Ok(_)   => Err(()), 
-                                        Err(..) => Err(())
-                              };
-                     let resend: Result<&Array<Ix1, T>, ()>  
-                            = match self.call_point(&Atom::Point {point: end}) {
-                                        Ok(Atom::Point { point: arr }) => Ok(arr),  
-                                        Ok(_)   => Err(()), 
-                                        Err(..) => Err(())
+              => { let resstart: Result<&Array<Ix1, T>, ()>
+                            = match self.call_point(&Atom::Point {point: start}) {
+                                      Ok(Atom::Point { point: arr }) => Ok(arr), 
+                                      Ok(_)   => Err(()), 
+                                      Err(..) => Err(())
                             };
-                     Ok(&Atom::Line { start: resstart.unwrap() , end: resend.unwrap() })
-                   },
+                   let resend: Result<&Array<Ix1, T>, ()>  
+                          = match self.call_point(&Atom::Point {point: end}) {
+                                      Ok(Atom::Point { point: arr }) => Ok(arr),  
+                                      Ok(_)   => Err(()), 
+                                      Err(..) => Err(())
+                          };
+                   let val = Atom::Line { start: &resstart.unwrap() , end: &resend.unwrap() };
+                   Ok(val)
+                 },
             _   => Err(()),
         };
         res
