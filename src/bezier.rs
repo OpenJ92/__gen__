c55d8cfa058ffd0914a743fb67e4885790d51165
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-02-27 18:54:57 -0500

    Work on bezier forms

--- src/bezier.rs
+++ src/bezier.rs
@@ -2,6 +2,9 @@ use crate::atomics::__Callable__;
 use ndarray::Array;
 use ndarray::IxDyn;
 
+
+// This will be the first implementation we carry out here. 
+// Remember to implement it as you have it in your blog post.
 struct Bezier<'a, B> {
     pub control_points: Array<IxDyn, B>,
     pub callparam: fn(__Callable__<'a, B>) -> __Callable__<'a, B>,
