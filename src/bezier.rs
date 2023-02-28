66e38f66a87f310ac6edffcce9a763002a63dbe5
OpenJ92 <jacob.vartuli.92@gmail.com>
2023-02-28 10:44:07 -0500

    playing around in ndarray

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,19 +1,31 @@
 use crate::traits::{VectorFunction};
 use crate::atomics::__Callable__;
 
-use ndarray::{ Array, IxDyn, Ix1 };
-
+use ndarray::{ Array, IxDyn, Ix1, Axis };
+use num_traits::real::Real;
 
 // This will be the first implementation we carry out here. 
 // Remember to implement it as you have it in your blog post.
 struct Bezier<T> {
     pub control_points: Array<T, IxDyn>,
-    pub collape_axes: Array<T, Ix1>,
+    pub collapse_axes: Array<T, Ix1>,
     pub callparam: fn(Array<T, Ix1>) -> Array<T, Ix1>,
+    // It's seeming like we're going to have to store the partial computation here or we'll have to
+    // make new control_points, collapse_axes and callparam in a new bezier and then call the
+    // updated t on it. 
 }
 
-impl<'a, T> VectorFunction<'a, T> for Bezier<T> {
+impl<'a, T> VectorFunction<'a, T> for Bezier<T> 
+where T: Real {
     fn call(&self, t: &'a mut Array<T, IxDyn>) -> () {
-        todo!()
+        let (x, xs) = t.view().split_at(Axis(0), 1);
+        let (c, cs) = self.collapse_axes.view().split_at(Axis(0), 1);
+
+        self.control_points.view().axis_iter(Axis(c.first().unwrap().to_usize().unwrap()));
+
+        if cs.is_empty() { todo!(); }
+
+
+        *t = xs.to_owned().into_dyn();
     }
 }
